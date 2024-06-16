0a22b7e3b6217d066e2ad0d3d3515dc0b720cf7e
Edyta <edysiaborowska27@gmail.com>
2024-06-16 16:50:20 +0200

    Moved implementation of the lotto game from Main to MiniLotto

--- src/main/java/pl/edytaborowska/Main.java
+++ src/main/java/pl/edytaborowska/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Wybierz grę do uruchomienia(np. lotto): ");
+        System.out.println("Select a game to launch(e.g. lotto): ");
         String gameType = sc.nextLine();
         try {
             Game game = GameFactory.getGame(gameType);
@@ -18,59 +18,12 @@ public class Main {
             System.out.println(e.getMessage());
         }
 
-        List<Integer> userNumbers = new ArrayList();
 
-        boolean error = true;
-
-        for (int i = 0; i < 6; ++i) {
-            try {
-                int nextNumber = sc.nextInt();
-                error = false;
-                userNumbers.add(nextNumber);
-            } catch (InputMismatchException ex) {
-                System.out.println("The value provided is not an integer");
-                sc.nextLine();
-                i--;
-            }
-            while (error) ;
-
-            if (userNumbers.get(i) < 1) {
-                System.out.println("You entered a number less than 1");
-                userNumbers.remove(i);
-                i--;
-
-
-            } else {
-                if (userNumbers.get(i) > 99) {
-                    System.out.println("You entered a number greater than 99");
-                    userNumbers.remove(i);
-                    i--;
-
-                }
-            }
-
-
-        }
         sc.close();
 
-        if (userNumbers.size() != 6) System.out.println("You didn't provide 6 numbers, try again");
 
-        for (int i = 0; i < 6; i++) {
-            if (0 > userNumbers.get(i)) {
-                if (userNumbers.get(i) > 100)
-                    System.out.println("You have entered numbers greater than or less than the set of integers from 1 to 99, try again");
-            }
-        }
-        DrawingNumbers win = new DrawingNumbers();
-        List<Integer> winNumbs = win.getWinNumbs();
-        int result = win.checkResult(userNumbers);
 
-        System.out.println("Here are the numbers drawn " + winNumbs);
 
-        Collections.sort(userNumbers);
-        System.out.println("The number of your hits " + result);
-
-        System.out.println(win.isEqual(userNumbers));
 
 
     }
--- src/main/java/pl/edytaborowska/MiniLotto.java
+++ src/main/java/pl/edytaborowska/MiniLotto.java
@@ -1,7 +1,6 @@
 package pl.edytaborowska;
 
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
 
 public class MiniLotto implements Game{
     @Override
@@ -10,6 +9,53 @@ public class MiniLotto implements Game{
         System.out.println("Hello! I invite you to play mini lotto, guess 6 numbers from 1 to 99. ");
         System.out.println("Enter 6 numbers separated by a space and confirm with enter.");
         //Przykładowa implementacja gry lotto
+        Scanner sc = new Scanner(System.in);
+
+        List<Integer> userNumbers = new ArrayList();
+
+        boolean error = true;
+
+        for (int i = 0; i < 6; ++i) {
+            try {
+                int nextNumber = sc.nextInt();
+                error = false;
+                userNumbers.add(nextNumber);
+            } catch (InputMismatchException ex) {
+                System.out.println("The value provided is not an integer");
+                sc.nextLine();
+                i--;
+            }
+            while (error) ;
+
+            if (userNumbers.get(i) < 1) {
+                System.out.println("You entered a number less than 1");
+                userNumbers.remove(i);
+                i--;
+
+
+            } else {
+                if (userNumbers.get(i) > 99) {
+                    System.out.println("You entered a number greater than 99");
+                    userNumbers.remove(i);
+                    i--;
+
+                }
+            }
+
+
+        }
+        sc.close();
+        DrawingNumbers win = new DrawingNumbers();
+        List<Integer> winNumbs = win.getWinNumbs();
+        int result = win.checkResult(userNumbers);
+
+        System.out.println("Here are the numbers drawn " + winNumbs);
+
+        Collections.sort(userNumbers);
+        System.out.println("The number of your hits " + result);
+
+        System.out.println(win.isEqual(userNumbers));
+
 
     }
 
